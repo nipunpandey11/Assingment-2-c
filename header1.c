f877f45b479427abd7be78308654a377155bface
nipunpandey11 <nipunpandey80@gmail.com>
2025-04-30 00:09:38 +0530

    Create header1.c

---
+++ header1.c
@@ -0,0 +1,19 @@
+for(int i =1;i<=a;i++){ 
+         b+=1;
+         }
+         if(b==1){
+             return 1;
+         }
+         if(b==2){
+             return 2;
+         }
+         if(b==3){
+             return 3;
+         }
+         if(b==4){
+             return 4;
+         }
+         return b;
+ 
+     }     
+ 
